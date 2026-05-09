ee6c365105149f234338758f123eb859219b291e
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2026-05-10 02:57:12 +0600

    :sparkles: add landing page (#3)

---
+++ app/layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <UHeader title="Travel Log" :toggle="false">
+            <template #right>
+                <UColorModeButton>
+                    <template #fallback>
+                        <UButton
+                            loading
+                            variant="ghost"
+                            color="neutral"
+                        />
+                    </template>
+                </UColorModeButton>
+
+                <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
+                    <UButton
+                        color="neutral"
+                        variant="ghost"
+                        to="https://github.com/frost23z/travel-log"
+                        target="_blank"
+                        icon="i-simple-icons-github"
+                        aria-label="GitHub"
+                    />
+                </UTooltip>
+                <UButton color="primary" variant="solid">
+                    Sign In
+                </UButton>
+            </template>
+        </UHeader>
+
+        <div class="flex min-h-[calc(100vh-var(--ui-header-height))] flex-col justify-between">
+            <UContainer><slot /></UContainer>
+            <div>
+                <USeparator
+                    icon="tabler:keyframe-align-horizontal"
+                    type="dotted"
+                    size="sm"
+                    class="relative top-2.5"
+                />
+                <UFooter>
+                    <p class="text-sm text-muted">
+                        Copyright © {{ new Date().getFullYear() }} Travel Log. All rights reserved.
+                    </p>
+                </UFooter>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { ButtonProps } from "@nuxt/ui"
+
+const links = ref<ButtonProps[]>([
+    {
+        label: "Get started",
+        to: "/docs/getting-started",
+        icon: "tabler:login-2",
+    },
+    {
+        label: "Learn more",
+        to: "/docs/getting-started/theme/design-system",
+        color: "neutral",
+        variant: "subtle",
+        trailingIcon: "i-lucide-arrow-right",
+    },
+])
+</script>
+
+<template>
+    <UPageHero
+        headline="New release"
+        title="Travel Log"
+        description="Keep track of your travels and adventures with Travel Log, a simple and elegant travel journal app built with Nuxt 4 and Nuxt UI."
+        :links="links"
+    />
+</template>
