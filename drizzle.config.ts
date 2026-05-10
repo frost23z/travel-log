12e9776b75a3f9d1afee930843a917d7a1d13f7f
frost23z <80541095+frost23z@users.noreply.github.com>
2026-05-10 08:26:07 +0600

    ✨ initialize Drizzle ORM with Turso
    - added libsql client setup
    - defined initial schema
    - configured drizzle-kit
    - enabled local SQLite dev via Turso CLI

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit"
+
+import env from "./server/env"
+
+export default defineConfig({
+    out: "./server/db/migrations",
+    schema: "./server/db/schema",
+    casing: "snake_case",
+    dialect: "turso",
+    dbCredentials: {
+        url: env.TURSO_DATABASE_URL,
+        authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
+    },
+})
