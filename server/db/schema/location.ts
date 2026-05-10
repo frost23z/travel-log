3efd12d4cbcf7df4251cd2116f844f01e1a09326
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2026-05-10 21:27:51 +0600

    ✨ set up better auth (#6)

--- server/db/schema/location.ts
+++ server/db/schema/location.ts
@@ -1,13 +1,15 @@
-import { real } from "drizzle-orm/gel-core"
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core"
+
+import { user } from "./auth"
 
 export const location = sqliteTable("location", {
-    id: int().primaryKey({ autoIncrement: true }),
+    id: text().primaryKey(),
     name: text().notNull(),
     slug: text().notNull().unique(),
     description: text(),
     lat: real().notNull(),
     lng: real().notNull(),
+    userId: text().notNull().references(() => user.id, { onDelete: "cascade" }),
     createdAt: int().notNull().$default(() => Date.now()),
     updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 })
