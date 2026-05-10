0878b1c8beb9c2e863c06d6ddcb6d31856271a18
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2026-05-11 05:21:51 +0600

    :sparkles: implement GitHub OAuth authentication (#7)
    - add login with GitHub
    - implement logout functionality
    - display authentication state in UI

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,48 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from "@nuxt/ui"
+
+const year = new Date().getFullYear()
+
+const useAuth = useAuthStore()
+
+const user = useAuth.user
+
+const items = ref<DropdownMenuItem[][]>([
+    [
+        {
+            label: user?.name,
+            avatar: {
+                src: user?.image || undefined,
+                loading: "lazy",
+            },
+            type: "label",
+        },
+    ],
+    [
+        {
+            label: "Profile",
+            icon: "i-lucide-user",
+        },
+        {
+            label: "Settings",
+            icon: "i-lucide-cog",
+            kbds: [","],
+        },
+    ],
+    [
+        {
+            label: "Logout",
+            icon: "i-lucide-log-out",
+            color: "error",
+            kbds: ["shift", "meta", "q"],
+            onSelect: () => {
+                useAuth.signOut()
+            },
+        },
+    ],
+])
+</script>
+
 <template>
     <div>
         <UHeader title="Travel Log" :toggle="false">
@@ -22,14 +67,23 @@
                         aria-label="GitHub"
                     />
                 </UTooltip>
-                <UButton color="primary" variant="solid">
+                <UDropdownMenu v-if="useAuth.user" :items="items">
+                    <UAvatar :src="user?.image || undefined" :alt="user?.name" />
+                </UDropdownMenu>
+                <UButton
+                    v-else
+                    color="primary"
+                    variant="solid"
+                    to="/login"
+                >
                     Sign In
                 </UButton>
             </template>
         </UHeader>
 
         <div class="flex min-h-[calc(100vh-var(--ui-header-height))] flex-col justify-between">
-            <UContainer><slot /></UContainer>
+            <main><UContainer><slot /></UContainer></main>
+
             <div>
                 <USeparator
                     icon="tabler:keyframe-align-horizontal"
@@ -39,7 +93,7 @@
                 />
                 <UFooter>
                     <p class="text-sm text-muted">
-                        Copyright © {{ new Date().getFullYear() }} Travel Log. All rights reserved.
+                        Copyright © {{ year }} Travel Log. All rights reserved.
                     </p>
                 </UFooter>
             </div>
